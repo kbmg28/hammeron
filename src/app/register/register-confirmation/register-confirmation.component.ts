218754db570d9f13865b31b033ae19875f95198c
Kevin Gomes <kb.developer.br@gmail.com>
2021-08-31 10:54:23 -0400

    register confirmation with translate

--- src/app/register/register-confirmation/register-confirmation.component.ts
+++ src/app/register/register-confirmation/register-confirmation.component.ts
@@ -1,3 +1,4 @@
+import { LocalizationService } from './../../internationalization/localization.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterConfirmationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private localizationService: LocalizationService) { }
 
   ngOnInit(): void {
+  }
+
+  sendNewToken() {
+    console.log('send new token...')
   }
 
 }
