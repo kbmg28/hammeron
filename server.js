c7082c3a0a9ad0731011c1a72b5a84c94734278b
Kevin Gomes <kb.developer.br@gmail.com>
2021-08-31 11:16:05 -0400

    setting heroku

---
+++ server.js
@@ -0,0 +1,17 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const appName = process.env.npm_package_name;
+
+// Serve only the static files form the dist directory
+app.use(express.static(`./dist/${appName}`));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: `dist/${appName}`}),
+);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 9000);
